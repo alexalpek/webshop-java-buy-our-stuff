65d9b803460db06d599d5a72a02dd83f319ee8a6
David Schmidt <david.adam.schmidt@gmail.com>
2019-09-05 21:24:26 +0200

    move order processing logic from CheckoutController to PaymentController

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -3,9 +3,6 @@ package com.codecool.shop.controller;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.dao.implementation.CartDaoMem;
-import com.codecool.shop.model.Cart;
-import com.codecool.shop.model.Order;
-import com.codecool.shop.model.ShippingInfo;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -25,23 +22,7 @@ public class CheckoutController extends HttpServlet {
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("cartSize", cartDataStore.find(1).size());
+        context.setVariable("cart", cartDataStore.find(1));
         engine.process("product/checkout.html", context, resp.getWriter());
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("name");
-        String email = req.getParameter("email");
-        String phoneNumber = req.getParameter("phone-number");
-        String billingAddress = req.getParameter("billing-address");
-        String shippingAddress = req.getParameter("shipping-address");
-
-        ShippingInfo shippingInfo = new ShippingInfo(name, email, phoneNumber, billingAddress, shippingAddress);
-        CartDao cartDataStore = CartDaoMem.getInstance();
-        int cartId = Integer.parseInt(req.getParameter("cart"));
-        Cart cart = cartDataStore.find(cartId);
-
-        Order order = new Order(cart, shippingInfo);
     }
 }
