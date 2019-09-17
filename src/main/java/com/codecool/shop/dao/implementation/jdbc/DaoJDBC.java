83af8d1975704a8f11674a7df8932766e06e0600
Mátrai Barnabás <matrai.barnabas1@gmail.com>
2019-09-18 01:44:32 +0200

    database uses data from file

--- src/main/java/com/codecool/shop/dao/implementation/jdbc/DaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/jdbc/DaoJDBC.java
@@ -1,8 +1,10 @@
 package com.codecool.shop.dao.implementation.jdbc;
 
+import com.codecool.shop.config.ConfigFileReader;
 import lombok.Cleanup;
 
 import java.sql.*;
+import java.util.Map;
 
 public abstract class DaoJDBC {
 
@@ -11,9 +13,25 @@ public abstract class DaoJDBC {
     private static final String PASSWORD;
 
     static {
-        USERNAME = System.getenv("username");
-        DATABASE = System.getenv("database");
-        PASSWORD = System.getenv("password");
+        ConfigFileReader cfr = new ConfigFileReader();
+        Map<String, String> databaseData = null;
+
+        try {
+            databaseData = cfr.readDataFromFile("connection.properties");
+        }
+        catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        if (databaseData != null) {
+            USERNAME = databaseData.get("user");
+            DATABASE = databaseData.get("url") + databaseData.get("database");
+            PASSWORD = databaseData.get("password");
+        } else {
+            USERNAME = System.getenv("username");
+            DATABASE = System.getenv("database");
+            PASSWORD = System.getenv("password");
+        }
     }
 
     Connection getConnection() throws SQLException {
