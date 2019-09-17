aabce92f5fe60b42daa455830a66e217887ccdaa
Mátrai Barnabás <matrai.barnabas1@gmail.com>
2019-09-17 15:36:47 +0200

    correct file reading functionality

--- src/main/java/com/codecool/shop/Read.java
+++ src/main/java/com/codecool/shop/Read.java
@@ -3,35 +3,49 @@ package com.codecool.shop;
 import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 public class Read {
 
     public Read() {
-        readDataFromFile("connection.properties");
+        try {
+            readDataFromFile("connection.properties");
+        }
+        catch (ClassNotFoundException e) {
+            System.err.println("Class not found");
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Read read = new Read();
     }
 
-    public List<String> readDataFromFile(String filename) {
+    public List<String> readDataFromFile(String filename) throws ClassNotFoundException{
 
         StringBuilder sb = new StringBuilder();
 
-        try (BufferedReader br = Files.newBufferedReader(Paths.get(filename))) {
+        ClassLoader cl = Class.forName("com.codecool.shop.Read").getClassLoader();
+        URL url = cl.getResource("connection.properties");
+        String file;
+
+        if (url.getFile() != null) {
+            file = url.getFile();
+        } else {
+            return new ArrayList<String>();
+        }
+
+        try (FileReader reader = new FileReader(file);
+             BufferedReader br = new BufferedReader(reader)) {
 
             String line;
             while ((line = br.readLine()) != null) {
                 sb.append(line.split(" ")[1]).append(" ");
             }
-
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             System.err.format("IOException: %s%n", e);
         }
 
