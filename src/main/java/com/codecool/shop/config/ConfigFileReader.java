ac52482bf1fc0949458ee1b18e122f2313b297cc
Mátrai Barnabás <matrai.barnabas1@gmail.com>
2019-09-18 01:28:36 +0200

    switch from List to Map in ConfigFileReader

--- src/main/java/com/codecool/shop/config/ConfigFileReader.java
+++ src/main/java/com/codecool/shop/config/ConfigFileReader.java
@@ -2,27 +2,12 @@ package com.codecool.shop.config;
 
 import java.io.*;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConfigFileReader {
 
-    public ConfigFileReader() {
-        try {
-            readDataFromFile("connection.properties");
-        }
-        catch (ClassNotFoundException e) {
-            System.err.println("Class not found");
-        }
-    }
-
-    public static void main(String[] args) {
-        ConfigFileReader read = new ConfigFileReader();
-    }
-
-    public List<String> readDataFromFile(String filename) throws ClassNotFoundException{
-
-        StringBuilder sb = new StringBuilder();
+    public Map<String, String> readDataFromFile(String filename) throws ClassNotFoundException{
 
         ClassLoader cl = Class.forName("com.codecool.shop.config.ConfigFileReader").getClassLoader();
         URL url = cl.getResource(filename);
@@ -34,20 +19,21 @@ public class ConfigFileReader {
             return null;
         }
 
+        Map<String, String> databaseData = new HashMap<>();
+
         try (FileReader reader = new FileReader(file);
              BufferedReader br = new BufferedReader(reader)) {
 
             String line;
             while ((line = br.readLine()) != null) {
-                sb.append(line.split(" ")[1]).append(" ");
+                String[] lineData = line.split(": ");
+                databaseData.put(lineData[0], lineData[1]);
             }
         }
         catch (IOException e) {
             System.err.format("IOException: %s%n", e);
         }
 
-        List<String> data = Arrays.asList(sb.toString().trim().split(" "));
-
-        return data;
+        return databaseData;
     }
 }
