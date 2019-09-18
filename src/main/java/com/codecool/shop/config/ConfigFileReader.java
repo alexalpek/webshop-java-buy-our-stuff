314293874f748bfd34b6574377f1cfa7098fab19
Mátrai Barnabás <matrai.barnabas1@gmail.com>
2019-09-18 02:14:11 +0200

    rewrite file reading to appropriate form

--- src/main/java/com/codecool/shop/config/ConfigFileReader.java
+++ src/main/java/com/codecool/shop/config/ConfigFileReader.java
@@ -1,34 +1,43 @@
 package com.codecool.shop.config;
 
 import java.io.*;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class ConfigFileReader {
+
+    public ConfigFileReader() {
+        try {
+            readDataFromFile("connection.properties");
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ConfigFileReader cfr = new ConfigFileReader();
+    }
 
     public Map<String, String> readDataFromFile(String filename) throws ClassNotFoundException{
 
         ClassLoader cl = Class.forName("com.codecool.shop.config.ConfigFileReader").getClassLoader();
-        URL url = cl.getResource(filename);
-        String file;
-
-        if (url != null) {
-            file = url.getFile();
-        } else {
-            return null;
-        }
-
         Map<String, String> databaseData = new HashMap<>();
 
-        try (FileReader reader = new FileReader(file);
-             BufferedReader br = new BufferedReader(reader)) {
+        try {
+            InputStream input = cl.getResourceAsStream(filename);
 
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] lineData = line.split(": ");
-                databaseData.put(lineData[0], lineData[1]);
+            Properties prop = new Properties();
+
+            if (input != null) {
+                prop.load(input);
             }
+
+            databaseData.put("url", prop.getProperty("url"));
+            databaseData.put("database", prop.getProperty("database"));
+            databaseData.put("user", prop.getProperty("user"));
+            databaseData.put("password", prop.getProperty("password"));
         }
         catch (IOException e) {
             System.err.format("IOException: %s%n", e);
